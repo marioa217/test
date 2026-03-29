4f39b20140892668a998da9da52657a6b8a1258f
marioa217 <mario.a217217@gmail.com>
2026-03-29 11:22:22 +0300

    Update pos_sgr.js

--- pos_sgr_romania/static/src/js/pos_sgr.js
+++ pos_sgr_romania/static/src/js/pos_sgr.js
@@ -1,21 +1,30 @@
 /** @odoo-module **/
-import { patch } from "@web/core/utils/patch";
+
 import { Order } from "@point_of_sale/app/store/models";
+import { patch } from "@web/core/utils/patch";
 
 patch(Order.prototype, {
     async add_product(product, options) {
+        // 1. First, call the original function
         await super.add_product(...arguments);
-        if (product.tt_has_sgr) {
-            const sgrProduct = this.pos.db.search_product_in_category(0)
-                .find(p => p.default_code === 'SGR');
+
+        // 2. Safety check: does the product exist and does it need SGR?
+        if (product && product.tt_has_sgr) {
+            // Find the SGR product in the POS database
+            const sgrProduct = this.pos.db.get_product_by_barcode('SGR') || 
+                               this.pos.db.search_product_in_category(0).find(p => p.default_code === 'SGR');
+
             if (sgrProduct) {
-                const qty = product.tt_sgr_qty || 1;
+                const sgrQty = product.tt_sgr_qty || 1;
+                // Add the SGR line silently
                 await super.add_product(sgrProduct, {
-                    quantity: qty,
+                    quantity: sgrQty,
                     merge: true,
                     silent: true,
                 });
+            } else {
+                console.warn("SGR Product not found! Make sure Internal Reference is 'SGR'");
             }
         }
-    }
+    },
 });
