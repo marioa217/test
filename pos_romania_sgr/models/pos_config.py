dff044bed55f020c125434db90d7343f428ddb14
marioa217 <mario.a217217@gmail.com>
2026-03-29 23:26:55 +0300

    Update pos_config.py

--- pos_romania_sgr/models/pos_config.py
+++ pos_romania_sgr/models/pos_config.py
@@ -1,18 +1,9 @@
-from odoo import api, fields, models
-
+from odoo import fields, models
 
 class PosConfig(models.Model):
     _inherit = "pos.config"
-
+    
     sgr_product_id = fields.Many2one(
         comodel_name="product.product",
         string="SGR Fee Product",
     )
-
-    @api.model
-    def _load_pos_data_fields(self, config):
-        fields_list = super()._load_pos_data_fields(config)
-        if "sgr_product_id" not in fields_list:
-            fields_list.append("sgr_product_id")
-        return fields_list
-
