6cb88a855aa1f317481242bdaa966d76f4b3a73e
marioa217 <mario.a217217@gmail.com>
2026-03-29 11:43:06 +0300

    Update product_template.py

--- pos_sgr_romania/models/product_template.py
+++ pos_sgr_romania/models/product_template.py
@@ -10,3 +10,12 @@ class ProductTemplate(models.Model):
         ('metal', 'Garantie SGR Metal'),
     ], string="Tip garantie", default='plastic')
     tt_sgr_qty = fields.Integer(string="Nr. ambalaje SGR", default=1)
+
+class PosSession(models.Model):
+    _inherit = 'pos.session'
+
+    def _loader_params_product_product(self):
+        result = super()._loader_params_product_product()
+        # This is critical: it sends the fields to the POS browser
+        result['search_params']['fields'].extend(['tt_has_sgr', 'tt_sgr_qty', 'default_code'])
+        return result
