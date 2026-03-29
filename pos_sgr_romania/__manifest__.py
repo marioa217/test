a3ae1a3c6a23e84061a2c682f5c4887e2a00de8e
marioa217 <mario.a217217@gmail.com>
2026-03-29 11:28:13 +0300

    Update __manifest__.py

--- pos_sgr_romania/__manifest__.py
+++ pos_sgr_romania/__manifest__.py
@@ -4,8 +4,10 @@
     'category': 'Point of Sale',
     'depends': ['point_of_sale', 'product'],
     'data': ['views/product_view.xml'],
-    'assets': {
-        'point_of_sale._assets_pos': ['pos_sgr_romania/static/src/js/pos_sgr.js'],
+'assets': {
+        'point_of_sale.assets': [
+            'pos_sgr_romania/static/src/js/pos_sgr.js',
+        ],
     },
     'installable': True,
     'license': 'LGPL-3',
