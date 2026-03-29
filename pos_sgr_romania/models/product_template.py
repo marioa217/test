742ae9ff1ee20cff59702eb165a74f4cdbb7b3bf
marioa217 <mario.a217217@gmail.com>
2026-03-29 16:53:27 +0300

    Update product_template.py

--- pos_sgr_romania/models/product_template.py
+++ pos_sgr_romania/models/product_template.py
@@ -3,18 +3,17 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    is_sgr = fields.Boolean(
-        string="Subject to SGR (0.50 RON)", 
-        default=False
-    )
+    is_sgr = fields.Boolean(string="Subject to SGR (0.50 RON)", default=False)
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
+    # This pulls the field down to the variant level (crucial for POS)
     is_sgr = fields.Boolean(related='product_tmpl_id.is_sgr', store=True, readonly=False)
 
     @api.model
     def _load_pos_data_fields(self, config_id):
+        """ Odoo 19 requires this to push custom fields to the frontend """
         fields = super()._load_pos_data_fields(config_id)
         if 'is_sgr' not in fields:
             fields.append('is_sgr')
