8b0582cc6504ff786544f088b87e293cf4472178
marioa217 <mario.a217217@gmail.com>
2026-03-29 16:43:29 +0300

    Update product_template.py

--- pos_sgr_romania/models/product_template.py
+++ pos_sgr_romania/models/product_template.py
@@ -1,15 +1,23 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     is_sgr = fields.Boolean(
         string="Subject to SGR (0.50 RON)", 
-        default=False,
-        help="Check this if the product requires the 50 bani SGR fee."
+        default=False
     )
-    
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     is_sgr = fields.Boolean(related='product_tmpl_id.is_sgr', store=True, readonly=False)
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        fields = super()._load_pos_data_fields(config_id)
+        if 'is_sgr' not in fields:
+            fields.append('is_sgr')
+        if 'default_code' not in fields:
+            fields.append('default_code')
+        return fields
