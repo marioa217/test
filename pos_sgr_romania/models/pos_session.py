a8a18c8cab8e8d72496634111db4840a15e31d69
marioa217 <mario.a217217@gmail.com>
2026-03-29 16:34:20 +0300

    Create pos_session.py

---
+++ pos_sgr_romania/models/pos_session.py
@@ -0,0 +1,11 @@
+from odoo import models
+
+class PosSession(models.Model):
+    _inherit = 'pos.session'
+
+    def _loader_params_product_product(self):
+        result = super()._loader_params_product_product()
+        # Add our custom field to the list of fields loaded in POS
+        if 'is_sgr' not in result['search_params']['fields']:
+            result['search_params']['fields'].append('is_sgr')
+        return result
