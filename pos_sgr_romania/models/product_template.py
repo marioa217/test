d5ec04992a363a1ca382fb3db811efc7f6e4ace0
marioa217 <mario.a217217@gmail.com>
2026-03-29 16:02:31 +0300

    Update product_template.py

--- pos_sgr_romania/models/product_template.py
+++ pos_sgr_romania/models/product_template.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -8,3 +8,13 @@ class ProductTemplate(models.Model):
         default=False,
         help="Check this if the product requires the 50 bani SGR fee."
     )
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        """ Odoo 18/19 specific method to load custom fields into the POS frontend """
+        fields = super()._load_pos_data_fields(config_id)
+        fields.append('is_sgr')
+        return fields
