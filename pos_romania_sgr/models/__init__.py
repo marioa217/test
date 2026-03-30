d1f31cd380dedd018da70cfbbef690ddd6172cf8
marioa217 <mario.a217217@gmail.com>
2026-03-30 12:20:36 +0300

    Delete pos_romania_sgr directory

--- pos_romania_sgr/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import product_template
-from . import pos_config
-from . import res_config_settings
-
--- pos_romania_sgr/models/pos_config.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class PosConfig(models.Model):
-    _inherit = "pos.config"
-    
-    sgr_product_id = fields.Many2one(
-        comodel_name="product.product",
-        string="SGR Fee Product",
-    )
