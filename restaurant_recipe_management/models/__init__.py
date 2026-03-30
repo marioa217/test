9c53e4ab2fced4487bf549145c8f2a3150ad8afa
marioa217 <mario.a217217@gmail.com>
2026-03-30 23:43:31 +0300

    Delete restaurant_recipe_management directory

--- restaurant_recipe_management/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import restaurant_recipe
-from . import restaurant_recipe_line
-from . import restaurant_recipe_consumption
-from . import product_template
-from . import pos_order
-from . import pos_order_line
-from . import pos_config
